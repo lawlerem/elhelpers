eeddfecf2de53b1d39de407b22c02ffa27c52134
Ethan Lawler <lawlerem@gmail.com>
2025-06-23 23:08:14 -0300

    Vectorized crl and icrl for much faster repeated use

--- R/parameter_transformations.R
+++ R/parameter_transformations.R
@@ -3,10 +3,15 @@
 #' When using crl, the output vector will have one additional element added to 
 #'     the end compared to the input vector to ensure probabilities add to 1.
 #' When using icrl, the last element of the input is discarded.
+#' Can be vectorized, in which case elements with the same (n-1) indices
+#'     will be treated as parameters for the same probability vector.
+#' For example if x is a 3 dimensional array, then the operation is applied to
+#'     each vector x[i, j, ].
 #'
 #' @param qpi 
 #'     An unbounded parameter vector to be transformed into a probability
-#'     vector.
+#'         vector.
+#'     
 #' @param p 
 #'     A probability vector to be transformed to an unbounded probability
 #'     vector.
@@ -15,34 +20,82 @@
 #' 
 #' @export
 crl<- function(qpi) {
-    if( requireNamespace("RTMB", quietly = TRUE) ) {
+    do_ad<- (
+            requireNamespace("RTMB", quietly = TRUE) && 
+            "advector" %in% class(qpi)
+        )
+    if( qpi |> dim() |> is.null() ) dim(qpi)<- c(1, qpi |> length())
+    if( do_ad ) {
         plogis<- RTMB::plogis
-        c<- RTMB::ADoverload("c")
     } else {
         plogis<- stats::plogis
     }
-    pi<- qpi |> plogis() |> c(1)
+    pi<- qpi |> plogis()
     qi<- 1 - pi
-    cqi<- qi |> head(-1) |> (\(x) c(1, x))() |> cumprod()
-    p<- pi * cqi
+
+    p_dim<- qpi |> dim()
+    p_dim[length(p_dim)]<- p_dim[length(p_dim)] + 1
+    p<- 0 |> array(dim = p_dim)
+
+    cqi<- 1 |> array(dim = p_dim |> head(-1))
+    if( do_ad ) {
+        p<- p |> RTMB::AD()
+        cqi<- cqi |> RTMB::AD()
+    } else {}
+    selector<- p_dim |>
+        head(-1) |>
+        lapply(seq) |>
+        expand.grid() |>
+        as.matrix()
+
+    for( i in p |> dim() |> tail(1) |> seq() ) {
+        if( i > 1 ) {
+            cqi[selector]<- cqi[selector] *  qi[selector |> cbind(i - 1)]
+        }
+        if( i == (p |> dim() |> tail(1)) ) {
+            p[selector |> cbind(i)]<- cqi[selector]
+            next
+        }
+        p[selector |> cbind(i)]<- pi[selector |> cbind(i)] * cqi[selector]
+    }
     return( p )
 }
 #' @rdname crl
 #' @export
 icrl<- function(p) {
-    pi<- 0 * p |> utils::head(-1)
-    for( i in pi |> seq_along() ) {
-        pi[i] <- p[i]
-        for( j in seq_len(i - 1) ) {
-            pi[i]<- pi[i] / (1 - pi[j])
-        }
-    }
-
-    if( requireNamespace("RTMB", quietly = TRUE) ) {
+    do_ad<- (
+            requireNamespace("RTMB", quietly = TRUE) && 
+            "advector" %in% class(p)
+        )
+    if( p |> dim() |> is.null() ) dim(p)<- c(1, p |> length())
+    if( do_ad ) {
         qlogis<- RTMB::qlogis
     } else {
         qlogis<- stats::qlogis
     }
-    qpi<- pi |> qlogis()
+    qpi_dim<- p |> dim()
+    qpi_dim[length(qpi_dim)]<- qpi_dim[length(qpi_dim)] - 1
+    qpi<- 0 |> array(dim = qpi_dim)
+
+    denom<- 1 |> array(dim = qpi_dim |> head(-1))
+    if( do_ad ) {
+        qpi<- qpi |> RTMB::AD()
+        denom<- cnp |> RTMB::AD()
+    } else {}
+    selector<- qpi_dim |>
+        head(-1) |>
+        lapply(seq) |>
+        expand.grid() |>
+        as.matrix()
+
+    for( i in p |> dim() |> tail(1) |> (\(x) x - 1)() |> seq() ) {
+        if( i > 1 ) {
+            denom[selector]<- denom[selector] - p[selector |> cbind(i - 1)]
+        }
+        qpi[selector |> cbind(i)]<- (
+                p[selector |> cbind(i)] / denom[selector]
+            ) |>
+            qlogis()
+    }
     return(qpi)
 }
